f2f69cbe8dfbec9031576fae6286d22640fb779a
Haser <deskatihonov@gmail.com>
2024-02-04 11:23:29 +0300

    Damage Type

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -2,6 +2,8 @@
 
 #include "STUHealthComponent.h"
 #include "GameFramework//Actor.h"
+#include "Dev/STUFireDamageType.h"
+#include "Dev/STUIceDamageType.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All);
 
@@ -29,4 +31,16 @@ void USTUHealthComponent::OnTakeAnyDamage(AActor *DamagedActor, float Damage, co
 {
     Health -= Damage;
     UE_LOG(LogHealthComponent, Display, TEXT("Damage: %f"), Damage);
+
+    if (DamageType)
+    {
+        if (DamageType->IsA<USTUFireDamageType>())
+        {
+            UE_LOG(LogHealthComponent, Display, TEXT("So hoot"));
+        }
+        else if (DamageType->IsA<USTUIceDamageType>())
+        {
+            UE_LOG(LogHealthComponent, Display, TEXT("So coold"));
+        }
+    }
 }
--- Source/ShootThemUp/Private/Dev/STUDevDamageActor.cpp
+++ Source/ShootThemUp/Private/Dev/STUDevDamageActor.cpp
@@ -26,6 +26,6 @@ void ASTUDevDamageActor::Tick(float DeltaTime)
     Super::Tick(DeltaTime);
 
     DrawDebugSphere(GetWorld(), GetActorLocation(), Radius, 24, SphereColor);
-    UGameplayStatics::ApplyRadialDamage(GetWorld(), Damage, GetActorLocation(), Radius, nullptr, {}, this, nullptr,
+    UGameplayStatics::ApplyRadialDamage(GetWorld(), Damage, GetActorLocation(), Radius, DamageType, {}, this, nullptr,
                                         DoFullDamage);
 }
